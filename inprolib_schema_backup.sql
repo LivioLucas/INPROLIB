cce1058c4a6f2b4eff09c273d071983893f296f4
LivioLucas <145092103+LivioLucas@users.noreply.github.com>
2024-12-04 12:50:25 -0300

    estrutura BD

--- inprolib_schema_backup.sql
+++ inprolib_schema_backup.sql
@@ -14,21 +14,123 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela inprolib_schema.curso
+CREATE TABLE IF NOT EXISTS `curso` (
+  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_curso` varchar(255) NOT NULL,
+  `id_coordenador` int(11) DEFAULT NULL,
+  `descricao_curso` varchar(50) DEFAULT NULL,
+  `codigo_curso` varchar(50) DEFAULT NULL,
+  `autorizacao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_curso`),
+  KEY `id_coordenador` (`id_coordenador`),
+  CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`id_coordenador`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela inprolib_schema.esqueci_senha
+CREATE TABLE IF NOT EXISTS `esqueci_senha` (
+  `id_solicitacao` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `data_solicitacao` datetime NOT NULL,
+  `status` enum('Ativo','Expirado') NOT NULL,
+  PRIMARY KEY (`id_solicitacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela inprolib_schema.funcao
+CREATE TABLE IF NOT EXISTS `funcao` (
+  `id_funcao` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_funcao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela inprolib_schema.logs
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) DEFAULT NULL,
+  `atividade` varchar(255) NOT NULL,
+  `data_hora` datetime NOT NULL,
+  PRIMARY KEY (`id_log`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela inprolib_schema.publicacao
+CREATE TABLE IF NOT EXISTS `publicacao` (
+  `id_publicacao` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `data_publicacao` timestamp NOT NULL,
+  `id_autor` int(11) DEFAULT NULL,
+  `id_curso` int(11) DEFAULT NULL,
+  `tipo` varchar(255) NOT NULL DEFAULT '',
+  `status` enum('Publicado','Desativado') NOT NULL,
+  `arquivo` varchar(255) DEFAULT NULL,
+  `nome_arquivo` varchar(255) DEFAULT NULL,
+  `assuntos_relacionados` varchar(255) DEFAULT NULL,
+  `data_autoria` date DEFAULT NULL,
+  PRIMARY KEY (`id_publicacao`),
+  KEY `id_autor` (`id_autor`),
+  KEY `id_curso` (`id_curso`),
+  CONSTRAINT `publicacao_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  CONSTRAINT `publicacao_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela inprolib_schema.relatorio
+CREATE TABLE IF NOT EXISTS `relatorio` (
+  `id_relatorio` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` enum('Por Curso','Por Data','Por Pessoa') NOT NULL,
+  `data_geracao` date NOT NULL,
+  PRIMARY KEY (`id_relatorio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela inprolib_schema.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('Funcionário','Professor','Coordenador','Secretaria','Bibliotecaria','Aluno') NOT NULL,
+  `foto_perfil` varchar(255) DEFAULT NULL,
+  `curso_usuario` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela inprolib_schema.usuario_curso
+CREATE TABLE IF NOT EXISTS `usuario_curso` (
+  `id_usuario` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_curso`),
+  KEY `id_curso` (`id_curso`),
+  CONSTRAINT `usuario_curso_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  CONSTRAINT `usuario_curso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela inprolib_schema.usuario_funcao
+CREATE TABLE IF NOT EXISTS `usuario_funcao` (
+  `id_usuario` int(11) NOT NULL,
+  `id_funcao` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_funcao`),
+  KEY `id_funcao` (`id_funcao`),
+  CONSTRAINT `usuario_funcao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  CONSTRAINT `usuario_funcao_ibfk_2` FOREIGN KEY (`id_funcao`) REFERENCES `funcao` (`id_funcao`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Exportação de dados foi desmarcado.
 
